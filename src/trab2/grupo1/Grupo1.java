2e56a8c913d56fe9ada062fc6c31b6123b9cd409
TitanOli <ruben.f.oliveira@gmail.com>
2021-01-06 16:00:36 +0000

    Grupo 1 finalizado

--- src/trab2/grupo1/Grupo1.java
+++ src/trab2/grupo1/Grupo1.java
@@ -1,9 +1,6 @@
 package trab2.grupo1;
 
-import javax.swing.*;
-import java.awt.*;
 import java.io.*;
-import java.util.concurrent.TimeUnit;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
@@ -12,19 +9,18 @@ public class Grupo1 {
     public static int replaceTabs(String pathnameIn, PrintWriter out, int n) throws IOException {
         int subs=0;
         String s=" ".repeat(n);
-        BufferedReader rd=new BufferedReader(new FileReader(pathnameIn));
-        int character=rd.read();
-        while(character!=-1){
-            if(((char)character)=='\t'){
-                out.print(s);
-                subs++;
+        try (BufferedReader rd=new BufferedReader(new FileReader(pathnameIn))) {
+            int character = rd.read();
+            while (character != -1) {
+                if (((char) character) == '\t') {
+                    out.print(s);
+                    subs++;
+                } else {
+                    out.print((char) character);
+                }
+                character = rd.read();
             }
-            else{
-                out.print((char) character);
-            }
-            character= rd.read();
         }
-        rd.close();
         return subs;
     }
 
@@ -42,27 +38,27 @@ public class Grupo1 {
     }
 
     public static int copyTeam(String pathnameIn, String teamId, BiConsumer<Integer, String> action) throws IOException {
-        BufferedReader rd=new BufferedReader(new FileReader(pathnameIn));
-        int subs=filter(rd, (str)->str.startsWith(teamId), (str)->{
-            int firstIdx=str.indexOf(' ');
-            int secondIdx=str.indexOf(' ', firstIdx+1);
+        try (BufferedReader rd=new BufferedReader(new FileReader(pathnameIn))) {
+            int subs = filter(rd, (str) -> str.startsWith(teamId), (str) -> {
+                        int firstIdx = str.indexOf(' ');
+                        int secondIdx = str.indexOf(' ', firstIdx + 1);
 
-            int number=Integer.parseInt(str.substring(firstIdx+1, secondIdx));
-            String name=str.substring(secondIdx+1);
+                        int number = Integer.parseInt(str.substring(firstIdx + 1, secondIdx));
+                        String name = str.substring(secondIdx + 1);
 
-            action.accept(number, name);
-        }
-        );
+                        action.accept(number, name);
+                    }
 
-        rd.close();
+            );
         return subs;
+        }
     }
 
     public static int copyTeam(String pathnameIn, String teamId) throws IOException{
-        PrintWriter out=new PrintWriter(new FileWriter(teamId+".txt"));
-        int subs = copyTeam(pathnameIn, teamId, (number, name)-> out.println(number+" "+name));
-        out.close();
-        return subs;
+        try (PrintWriter out=new PrintWriter(new FileWriter(teamId+".txt"))) {
+            int subs = copyTeam(pathnameIn, teamId, (number, name) -> out.println(number + " " + name));
+            return subs;
+        }
     }
 
     public static void printTeam(String pathnameIn, String teamId) throws IOException{
